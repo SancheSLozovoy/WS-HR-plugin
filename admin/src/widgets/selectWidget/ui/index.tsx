396732e26d802bea2d1fe3cd6c0ce556fbeb9a4c
SanchesLozovoy <alex.lozovoy05@bk.ru>
2024-10-18 14:53:36 +0300

    feat: add ability to copy task and alert component

--- admin/src/widgets/selectWidget/ui/index.tsx
+++ admin/src/widgets/selectWidget/ui/index.tsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Main, Button, SingleSelect, SingleSelectOption } from '@strapi/design-system';
+import { Main, Button, SingleSelect, SingleSelectOption, Box, Alert } from '@strapi/design-system';
 import { loadSpecialties, setSelectedSpecialty } from '../model/slices/selectSlice';
 import { AppDispatch, RootState } from 'src/app/store/store';
 import { getRandomTask } from '../../../features/getLink/model/slices/taskSlice';
+import { Crop } from '@strapi/icons';
 
 const Select = () => {
   const dispatch: AppDispatch = useDispatch();
   const { specialties, selectedSpecialty } = useSelector((state: RootState) => state.select);
   const { uuid, isValid, documentId } = useSelector((state: RootState) => state.taskLink);
+  const [alert, setAlert] = useState<boolean>(false);
+
+
+  const link = useRef<HTMLAnchorElement>(null);
 
   useEffect(() => {
     dispatch(loadSpecialties());
@@ -24,12 +29,23 @@ const Select = () => {
     }
   };
 
+  const handleCopyLink = () => {
+    if (link.current) {
+      const linkText = link.current.href;
+      navigator.clipboard.writeText(linkText)
+        .then(() => {
+          setAlert(true);
+          setTimeout(() => setAlert(false), 2000)
+        })
+    }
+  }
+
   return (
-    <Main style={{ width: "50%" }} className='main'>
-      <h1 style={{ fontSize: 32 }}>Выбор задания</h1>
+    <Main style={{ maxWidth: "733px" }} className='main'>
+      <h1 className='main__title' style={{ fontSize: "32px", fontWeight: 400, lineHeight: "39px", marginBottom: "25px" }}>Выбор задания</h1>
       {specialties.length !== 0 ? (
         <>
-          <h4 style={{ fontSize: 18, fontWeight: 300, marginBottom: 10}}>Специальность</h4>
+          <h4 style={{ fontSize: "14px", fontWeight: 400, marginBottom: "20px" }}>Специальность</h4>
           <SingleSelect
             value={selectedSpecialty || ''}
             onChange={handleSelectionChange}
@@ -41,24 +57,31 @@ const Select = () => {
               </SingleSelectOption>
             ))}
           </SingleSelect>
-          <Button style={{ marginTop: 10, fontSize: 43 }} onClick={handleGenerateLink}>
+          <Button style={{ marginTop: "25px" }} onClick={handleGenerateLink}>
             Сгенерировать ссылку на задание
           </Button>
 
           {isValid && uuid && (
-            <div style={{ fontSize: 18, marginTop: 20, fontWeight: 300 }}>
-              <p>Ссылка на задание: <a href={`http://localhost:3000/${documentId}`}>{`http://localhost:3000/${documentId}`}</a></p>
+            <div style={{ fontSize: "14px", marginTop: "25px", fontWeight: 400, lineHeight: "17px", }}>
+              <div style={{ marginBottom: "30px", display: 'flex', alignItems: 'center' }}>
+                <p style={{ marginRight: "15px" }}>Ссылка на страницу задания: <a ref={link} href={`http://localhost:3000/${documentId}`}>{`http://localhost:3000/${documentId}`}</a></p>
+                <Crop onClick={handleCopyLink} style={{ cursor: 'pointer' }} />
+              </div>
+              <p style={{ marginBottom: '10px' }}>Скопируйте ссылку, чтобы отправить ее соискателю.</p>
+              <p><span style={{ color: '#FE5815', fontWeight: 700 }}>Внимание!</span> Ссылка является одноразовой, поэтому не переходите по ней, во избежание ее инвалидации.</p>
             </div>
           )}
 
-          {!isValid && (
-            <div style={{ fontSize: 18, marginTop: 20, fontWeight: 300 }}>
-              <p>Нет доступной задачи или задача не загружена.</p>
-            </div>
+          {alert && (
+            <Box style={{ width: '100%', marginTop: '20px', transition: '1s'}}>
+              <Alert closeLabel="Закрыть уведомление">
+              Ссылка скопирована
+              </Alert>
+            </Box>
           )}
         </>
       ) : (
-        <div style={{ fontSize: 18, marginTop: 20, fontWeight: 300 }}>
+        <div style={{ fontSize: "14px", marginTop: "25px", fontWeight: 400, lineHeight: "17px" }}>
           Для выбора задания необходимо добавить хотя бы одну активную специальность и хотя бы одно активное задание для неё.
         </div>
       )}
